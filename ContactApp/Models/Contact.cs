ac4b2a47b09b5fae5b092a5f3ebf8070e834339f
ufuk çöz <ufuk200446@gmail.com>
2026-08-30 13:50:19 +0300

    ContactApp dosyalari kalici olarak eklendi

---
+++ ContactApp/Models/Contact.cs
@@ -0,0 +1,16 @@
+﻿namespace ContactApp.Models;
+    public class Contact
+    {
+
+        public int Id { get; set; }
+    public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+        public string? Company { get; set; }
+        public string? Title { get; set; }
+        public string? Notes { get; set; }
+
+
+}
+
---
+++ ContactApp/Services/Interface.cs
@@ -0,0 +1,14 @@
+﻿using ContactApp.Models;
+
+namespace ContactApp.Services
+{
+    public interface IContactRepository
+    {
+        IEnumerable<Contact> GetAllContacts();
+        Contact? GetById(int id);
+        Contact Add(Contact contact);
+        bool Update(Contact contact);
+        bool Delete(int id);
+
+    }
+}
