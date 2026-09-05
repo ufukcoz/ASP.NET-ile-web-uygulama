da255dcb1efd3040dd8e4e55b131783bfce7cbd7
ufuk çöz <ufuk200446@gmail.com>
2026-09-05 13:52:45 +0300

    News projesi eklendi

---
+++ News/Repositories/NewDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using News.Models;
+using News.Repositories.Config;
+using System.Diagnostics;
+using System.Reflection.Emit;
+
+namespace News.Repositories;
+
+public class NewsDbContext : DbContext
+{
+    public NewsDbContext(DbContextOptions<NewsDbContext> options)
+        : base(options)
+    {
+
+    }
+
+    public DbSet<NewsArticle> NewsArticles => Set<NewsArticle>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfiguration(new NewsArticleConfiguration());
+    }
+}
